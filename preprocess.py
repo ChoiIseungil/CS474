83877b27f681175f803decbb5c4fc9ad0ce818ff
ChoiIseungil <silly5921@kaist.ac.kr>
2021-11-11 21:40:36 +0900

    preprocessing added:

--- preprocess.py
+++ preprocess.py
@@ -1,23 +1,36 @@
 # Written by Seungil Lee, Nov 11, 2021
 
 import json
-import os
+import re
 import glob
+import nltk
 
-files = []
-for file in glob.glob("./raw_data/*.json"): files.append(file)
+stopwords = nltk.corpus.stopwords.words('english')
+
+def preprocess(text):
+    text = text.lower()
+    text = text.encode('ascii','ignore')
+    text = text.decode()                            #remove non ascii characters 
+    text = re.sub("[\[\(\{].*?[\]\)\}]", "", text)  #괄호 안 제거
+    text = re.sub(r'[^a-zA-Z\u00c0-\u00FF0-9\s]', '', text)
+    return text
 
 
-with open('./data.txt','w') as f:
-    for file in files:
-        j = open(file,'r')
-        data = json.load(j)
-        print(file)
-        for key in data: print(key)
-        titles = data["title"]
-        bodies = data["body"]
-        for key in titles:
-            f.write(titles[key] + ' ' + bodies[key] + '\n')
+def main():
+    files = []
+    for file in glob.glob("./raw_data/*.json"): files.append(file)
 
-f.close()
-# f = open()
+
+    with open('./data.txt','w') as f:
+        for file in files:
+            j = open(file,'r')
+            data = json.load(j)
+            titles = data["title"]
+            bodies = data["body"]
+            for key in titles:
+                f.write(titles[key] + ' ' + bodies[key] + '\n')
+
+    f.close()
+
+if __name__ == "__main__":
+    main()
