09be821d3954514265a6adcad42ff233de2984fd
ChoiIseungil <silly5921@kaist.ac.kr>
2021-11-16 20:38:35 +0900

    preprocess.py modified

--- preprocess.py
+++ preprocess.py
@@ -4,15 +4,20 @@ import json
 import re
 import glob
 import nltk
+import contractions
 
-stopwords = nltk.corpus.stopwords.words('english')
+nltk.download('stopwords')
+STOPWORDS = set(nltk.corpus.stopwords.words('english'))
+
 
 def preprocess(text):
-    text = text.lower()
-    text = text.encode('ascii','ignore')
-    text = text.decode()                            #remove non ascii characters 
-    text = re.sub("[\[\(\{].*?[\]\)\}]", "", text)  #괄호 안 제거
-    text = re.sub(r'[^a-zA-Z\u00c0-\u00FF0-9\s]', '', text)
+    text = re.sub("[\[\(\{].*?[\]\)\}]", "", text)                                      # 1. remove words in the parentheses
+    text = text.encode('ascii','ignore').decode()                                       # 2. remove non ascii characters
+    text = re.sub("b[w-.]+?@w+?.w{2,4}b", "", text)                                     # 3. remove email address
+    text = re.sub("(http[s]?S+)|(w+.[A-Za-z]{2,4}S*)", "", text)                        # 4. remove urls
+    text = text.lower()                                                                 # 5. lower
+    text = contractions.fix(text)                                                       # 6. expand contractions
+    text= " ".join([word for word in str(text).split() if word not in STOPWORDS])       # 7. remove stopwords
     return text
 
 
@@ -20,17 +25,21 @@ def main():
     files = []
     for file in glob.glob("./raw_data/*.json"): files.append(file)
 
+    print("Started")
 
     with open('./data.txt','w') as f:
         for file in files:
             j = open(file,'r')
             data = json.load(j)
-            titles = data["title"]
-            bodies = data["body"]
+            titles = preprocess(data["title"])
+            bodies = preprocess(data["body"])
             for key in titles:
                 f.write(titles[key] + ' ' + bodies[key] + '\n')
 
     f.close()
+
+    print("Finished")
+
 
 if __name__ == "__main__":
     main()
