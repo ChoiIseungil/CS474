d43a9a02512a1f92aa0280ec91cbdbdd57d7ba3b
ChoiIseungil <silly5921@kaist.ac.kr>
2021-11-21 18:07:41 +0900

    ner.py added by seungil

---
+++ ner.py
@@ -0,0 +1,21 @@
+# Written by Seungil Lee, Nov 21 2021
+# https://towardsdatascience.com/named-entity-recognition-with-nltk-and-spacy-8c4a7d88e7da
+# https://medium.com/@b.terryjack/nlp-pretrained-named-entity-recognition-7caa5cd28d7b
+
+import spacy
+sp_lg = spacy.load('en_core_web_lg')
+
+def ner(document):
+    return str({(ent.text.strip(), ent.label_) for ent in sp_lg(document).ents})
+
+raw = open('./data.txt','r')
+new = open('./ner.txt','w')
+
+
+for line in raw: 
+    new.write(ner(line))
+
+raw.close()
+new.close()
+
+
