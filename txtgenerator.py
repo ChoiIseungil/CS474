1866a75b6ee42ffde563fae8c927254095ccfc4a
ChoiIseungil <silly5921@kaist.ac.kr>
2021-11-25 03:15:27 +0900

    csv generator completed, preprocessing scheme slightly chagned

--- txtgenerator.py
+++ txtgenerator.py
@@ -16,14 +16,14 @@ def preprocess(text):
     text = re.sub("\S*@\S*\s?|(http[s]?S+)|(w+.[A-Za-z]{2,4}S*)", " ", text)            # 2. remove email address and ur.s
     text = text.lower()                                                                 # 3. lower
     text = re.sub("u.s."," usa ",text)                                                  # 4. corner case u.s. (appeared 15052 times..!)
-    text = text.replace("'s", " ")                                                      # 3. remove apostrophes with s (it appears a lot)
-    text = text.replace(".",". ")                                                       # 4. sentence seperate problem
-    text = text.encode('ascii','ignore').decode()                                       # 4. remove non ascii characters
-    text = contractions.fix(text)                                                       # 7. expand contractions
-    text = re.sub("[^\w. -]|_", " ", text)                                              # 8. erase except -, ., alphanumerics
-    text= " ".join([word for word in str(text).split() if word not in STOPWORDS])       # 9. remove stopwords
-    text = re.sub("north korea|n. korea","n.korea",text)                                # 11. corner case n.korea 
-    text = re.sub("south korea|s. korea","s.korea",text)                                # 12. corner case s.korea
+    text = text.replace("'s", " ")                                                      # 5. remove apostrophes with s (it appears a lot)
+    text = text.replace(".",". ")                                                       # 6. sentence seperate problem
+    text = text.encode('ascii','ignore').decode()                                       # 7. remove non ascii characters
+    text = contractions.fix(text)                                                       # 8. expand contractions
+    text = re.sub("[^\w. -]|_", " ", text)                                              # 9. erase except -, ., alphanumerics
+    text= " ".join([word for word in str(text).split() if word not in STOPWORDS])       # 10. remove stopwords
+    text = re.sub("north korea|n. korea","nkorea",text)                                # 11. corner case n.korea 
+    text = re.sub("south korea|s. korea","skorea",text)                                # 12. corner case s.korea
     return text
 
 
@@ -33,12 +33,14 @@ def main():
 
     with open('./data.txt','w') as f:
         for file in files:
+            print(file.name, " in progress")
             j = open(file,'r')
             data = json.load(j)
             titles = data["title"]
             bodies = data["body"]
             for key in titles:
                 f.write(preprocess(titles[key]) + ' ' + preprocess(bodies[key]) + '\n')
+            j.close()
 
     f.close()
 
