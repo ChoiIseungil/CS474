d97c06b49297e1386fbd3bc3a13159ba77f05256
ChoiIseungil <silly5921@kaist.ac.kr>
2021-11-30 03:19:02 +0900

    main.py on working

--- main.py
+++ main.py
@@ -7,30 +7,45 @@ input("""
     This is implemented for a term project of the course: 2021 Fall CS474 Text Mining, instructed by Prof. Sung-Hyon Myaeng
     Please contact silly5921@kaist.ac.kr if you have any feedback
 
-    Press enter to continue"""
+    Press Enter to continue
+    Press Ctrl+C any time you want to exit"""
     )
 
-YEAR = input("""
-    
-    
+while True:
+    YEAR = input("""
+         
     Choose the year you want to investigate among 2015, 2016, 2017
     Input: """)
 
-MODE = input("""
-    
-    
+    if YEAR not in ['2015','2016','2017']:
+        print("""
+    Only 2015, 2016, and 2017 are available years
+            """)
+
+    else: break
+
+while True:
+    MODE = input("""
+         
     Choose the mode
-    1. Issue Trend Analysis
-    2. On-issue Event Tracking
-    3. Related-issue Event Tracking 
-    Input: """)
+        1. Issue Trend Analysis
+        2. On-issue Event Tracking
+        3. Related-issue Event Tracking 
+        Input: """)
+
+    if MODE not in ['1','2','3']:
+        print("""
+    Input is not valid, please check once again
+        """)
+
+    else: break
 
 if MODE == 1:
     year = input("""
     Choose the mode
-    1. Issue Trend Analysis
-    2. On-issue Event Tracking
-    3. Related-issue Event Tracking 
+        1. Issue Trend Analysis
+        2. On-issue Event Tracking
+        3. Related-issue Event Tracking 
     Input: """)
 elif MODE == 2:
     pass
