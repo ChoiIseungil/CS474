0686722248c3576a58e68cb8a0a220e052346e5d
ChoiIseungil <silly5921@kaist.ac.kr>
2021-11-30 03:05:09 +0900

    main.py and run.sh added

---
+++ main.py
@@ -0,0 +1,38 @@
+# Written by Seungil Lee, Nov 30, 2021
+
+input("""
+    Welcome to Korean Issue Tracker 2015-2017,
+    Developed by Seungil Lee, Yunho Lee and Hyeann Lee
+
+    This is implemented for a term project of the course: 2021 Fall CS474 Text Mining, instructed by Prof. Sung-Hyon Myaeng
+    Please contact silly5921@kaist.ac.kr if you have any feedback
+
+    Press enter to continue"""
+    )
+
+YEAR = input("""
+    
+    
+    Choose the year you want to investigate among 2015, 2016, 2017
+    Input: """)
+
+MODE = input("""
+    
+    
+    Choose the mode
+    1. Issue Trend Analysis
+    2. On-issue Event Tracking
+    3. Related-issue Event Tracking 
+    Input: """)
+
+if MODE == 1:
+    year = input("""
+    Choose the mode
+    1. Issue Trend Analysis
+    2. On-issue Event Tracking
+    3. Related-issue Event Tracking 
+    Input: """)
+elif MODE == 2:
+    pass
+elif MODE == 3:
+    pass
--- tsv_generator.py
+++ tsv_generator.py
@@ -17,14 +17,14 @@ def preprocess(text):
     text = re.sub("\S*@\S*\s?|(http[s]?S+)|(w+.[A-Za-z]{2,4}S*)", " ", text)            # 2. remove email address and ur.s
     text = text.lower()                                                                 # 3. lower
     text = re.sub("u.s."," usa ",text)                                                  # 4. corner case u.s. (appeared 15052 times..!)
-    text = text.replace("'s", " ")                                                      # 5. remove apostrophes with s (it appears a lot)
-    text = text.replace(".",". ")                                                       # 6. sentence seperate problem
-    text = text.encode('ascii','ignore').decode()                                       # 7. remove non ascii characters
-    text = contractions.fix(text)                                                       # 8. expand contractions
-    text = re.sub("[^\w. -]|_", " ", text)                                              # 9. erase except -, ., alphanumerics
-    text= " ".join([word for word in str(text).split() if word not in STOPWORDS])       # 10. remove stopwords
-    text = re.sub("north korea|n. korea","nkorea",text)                                # 11. corner case n.korea 
-    text = re.sub("south korea|s. korea","skorea",text)                                # 12. corner case s.korea
+    text = re.sub("north korea|n. korea|n. k.","nkorea",text)                           # 5. corner case n.korea 
+    text = re.sub("south korea|s. korea|s. k.","skorea",text)                           # 6. corner case s.korea
+    text = text.replace("'s", " ")                                                      # 7. remove apostrophes with s (it appears a lot)
+    text = text.replace(".",". ")                                                       # 8. sentence seperate problem
+    text = text.encode('ascii','ignore').decode()                                       # 9. remove non ascii characters
+    text = contractions.fix(text)                                                       # 10. expand contractions
+    text = re.sub("[^\w. -]|_", " ", text)                                              # 11. erase except -, ., alphanumerics
+    text= " ".join([word for word in str(text).split() if word not in STOPWORDS])       # 12. remove stopwords
     return text
 
 
